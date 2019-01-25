c08f15be084977384b4083c1700166fcde29fc6a
stevejuUCL <46601911+stevejuUCL@users.noreply.github.com>
2019-01-25 15:26:47 +0000

    Add files via upload

---
+++ business_man.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 25, 2019 at 03:10 PM
+-- Server version: 5.7.24-log
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `business man`
+--
+CREATE DATABASE IF NOT EXISTS `business man` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `business man`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business man`
+--
+
+DROP TABLE IF EXISTS `business man`;
+CREATE TABLE `business man` (
+  `businessmanID` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `name` varchar(35) NOT NULL,
+  `contactnumber` int(35) NOT NULL,
+  `companyName` varchar(30) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `averagerating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `business man`
+--
+ALTER TABLE `business man`
+  ADD PRIMARY KEY (`businessmanID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `business man`
+--
+ALTER TABLE `business man`
+  MODIFY `businessmanID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
