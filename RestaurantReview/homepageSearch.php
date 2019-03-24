fd00c9b64b4ff9a2c55b7fd7fedad9a36a78f1a9
Steve Ju <steveju546@gmail.com>
2019-03-24 14:51:35 +0000

    17/02/2019

--- RestaurantReview/homepageSearch.php
+++ RestaurantReview/homepageSearch.php
@@ -1,3 +1,9 @@
+<title>Search</title>
+
+<?php
+require_once('header.php');
+?>
+<br><br><br><br><br>
 <?php
 /**
  * Created by PhpStorm.
@@ -10,4 +16,17 @@ require_once('PHP_Database/phpDatabaseConnection.php');
     $search = $_POST['Search'];
 
     $qrySearch = "SELECT * FROM restaurant WHERE name LIKE '%$Search%'";
+
+    $searchResult = mysqli_query($connection, $qrySearch);
+
+    if (mysqli_num_rows(￥searchResult) > 0) {
+        echo $searchResult;
+    }else {
+        echo "<h3 style='text-align:center'>Sorry, restaurant not found.😢<h3>";
+    }
 ?>
+<br><br><br><br><br>
+<?php
+require_once('footer.php');
+?>
+
